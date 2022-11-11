3545e263cd4e65444e1cd6e4645c5dc1848f3a22
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-11 15:24:32 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import {lazy} from "react";
 import  {Layout}  from "./components/Layout/Layout"
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-// const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
-// const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"));
 
 
 export const App = () => {
@@ -12,7 +12,10 @@ export const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-      <Route index path="/" element={<HomePage />} />
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Route> 
     </Routes> 
   );
