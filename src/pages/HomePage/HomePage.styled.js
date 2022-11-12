dc20eb9a2764525b0fd7f08bc87c92ba0836bb7e
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-12 16:52:36 +0300

    Update HomePage.styled.js

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -13,17 +13,19 @@ export const Container = styled.ul`
   padding-left: 15px;
   padding-right: 15px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr;
   gap: 10px;
   justify-content: center;
   @media screen and (min-width: 768px) {
     .container {
       width: 768px;
+      grid-template-columns: 1fr 1fr 1fr;
     }
   }
   @media screen and (min-width: 1200px) {
     .container {
       width: 1200px;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
     }
   }
 `;
